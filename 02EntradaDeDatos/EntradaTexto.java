50bf4443d4fd7ceedc22587602e1d3fe6538f49e
vanxito <jmendezr2101@ipn.mx>
2022-09-07 17:02:35 -0500

    Ejemplo de entrada de texto

---
+++ 02EntradaDeDatos/EntradaTexto.java
@@ -0,0 +1,26 @@
+class EntradaTexto{
+    public static void main (String[] args){
+
+        //los limites de la clase
+
+//toda la clase debe de tener un metodo principal
+
+//realizar un programa para introducir texto a nuestra conveniencia 
+
+//tipos de dato
+String nombre;
+//; es el fin de linea
+
+//instancia
+System.out.println("Por favor introduce tu nombre asi bien kawaii");
+//obtendremos el nombre del usuario
+nombre = System.console().readLine
+();
+
+System.out.println("Hola " + nombre + " bienvenido a tu programita");
+
+}
+
+
+    }
+    
---
+++ 02EntradaDeDatos/texto.java
@@ -0,0 +1,21 @@
+class EntradaTexto{
+//los limites de la clase
+
+//toda la clase debe de tener un metodo principal
+
+//realizar un programa para introducir texto a nuestra conveniencia 
+
+//tipos de dato
+String nombre;
+//; es el fin de linea
+
+//instancia
+System.out.println("Por favor introduce tu nombre asi bien kawaii");
+//obtendremos el nombre del usuario
+nombre = System.console().readLine
+();
+
+System.out.println("Hola" + nombre + "bienvenido a tu programita");
+
+}
+    
